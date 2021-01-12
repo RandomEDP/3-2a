a55fa81f3626eadc8ef2b92d52d4f22245fe7d2b
Ethan D-P <ethan.davison-pearce@ada.ac.uk>
2021-01-12 12:40:45 +0000

    Second exereise done

--- main.cpp
+++ main.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+// Workshop 3-2A
+// Exercise 1
+
+// int main() {
+//   string word;
+//   int wordLength;
+//   cout << "Please enter a word: \n";
+//   cin >> word;
+//   wordLength = word.length();
+//   int n=wordLength-1;
+//   for(int i=0;i<(wordLength/2);i++){
+//       swap(word[i],word[n]);
+//       n = n-1;
+//   }
+//   cout<<word;
+// }
+
+// Exercise 2
+
+int main(){
   string word;
-  int wordLength;
-  cout << "Please enter a word: \n";
+  cout << "Please enter a word: ";
   cin >> word;
-  wordLength = word.length();
-  int n=wordLength-1;
-  for(int i=0;i<(wordLength/2);i++){
-      swap(word[i],word[n]);
-      n = n-1;
+  string lowerCase = word;
+  for(int i = 0; i < lowerCase.length(); i++) { //step through every letter
+    lowerCase[i] = tolower(lowerCase[i]);
   }
-  cout<<word;
+  cout << lowerCase;
 }
+
+
 
