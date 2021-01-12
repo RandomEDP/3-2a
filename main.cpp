04fa7a6955b2ae76f66718f576230c88025af729
Ethan D-P <ethan.davison-pearce@ada.ac.uk>
2021-01-12 12:48:36 +0000

    Exercise 3

--- main.cpp
+++ main.cpp
@@ -20,16 +20,45 @@ using namespace std;
 
 // Exercise 2
 
+// int main(){
+//   string word;
+//   cout << "Please enter a word: ";
+//   cin >> word;
+//   string lowerCase = word;
+//   for(int i = 0; i < lowerCase.length(); i++) { //step through every letter
+//     lowerCase[i] = tolower(lowerCase[i]);
+//   }
+//   cout << lowerCase;
+// }
+
+// Exercise 3
+
 int main(){
   string word;
   cout << "Please enter a word: ";
   cin >> word;
-  string lowerCase = word;
+    string lowerCase = word;
   for(int i = 0; i < lowerCase.length(); i++) { //step through every letter
     lowerCase[i] = tolower(lowerCase[i]);
   }
-  cout << lowerCase;
+  int wordLength;
+  wordLength = word.length();
+  int n=wordLength-1;
+  string swapped = lowerCase;
+  for(int i=0;i<(wordLength/2);i++){
+      swap(swapped[i],swapped[n]);
+      n = n-1;
+  }
+
+  if(lowerCase == swapped){
+    cout << "The word " << word << " is a palindrome";
+  }
+  else{
+    cout << "The word " << word << " is not a palindrome";
+  }
+
 }
+
 
 
 
